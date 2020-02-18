825698ce6a89198aa72b6e5999e3aa6a5b8b6ca1
mkw <michaelkweiner@gmail.com>
2020-02-18 11:25:25 -0800

    license

--- dist/esm/definitions.d.ts
+++ dist/esm/definitions.d.ts
@@ -1,44 +1,12 @@
 declare module "@capacitor/core" {
-  interface PluginRegistry {
-    CapacitorTwilioVoiceSDK: CapacitorTwilioVoiceSDKPlugin;
-  }
+    interface PluginRegistry {
+        CapacitorTwilioVoiceSDK: CapacitorTwilioVoiceSDKPlugin;
+    }
 }
 export interface CapacitorTwilioVoiceSDKPlugin {
-    call(options: {
-    token: string;
-    params: any;
-  }): Promise<{
-    value: string;
-  }>;
-
-  sendDigits(options?: any): Promise<void>;
-
-  disconnect(options?: any): Promise<void>;
-
-  rejectCallInvite(options?: any): Promise<void>;
-
-  acceptCallInvite(options?: any): Promise<void>;
-
-  setSpeaker(options?: any): Promise<void>;
-  
-  muteCall(options?: any): Promise<void>;
-
-  unmuteCall(options?: any): Promise<void>;
-
-  isCallMuted(options?: any): Promise<void>;
-
-  error(options?: any): Promise<void>;
-
-  clientinitialized(options?: any): Promise<void>;
-
-  callinvitereceived(options?: any): Promise<void>;
-
-  callinvitecanceled(options?: any): Promise<void>;
-
-  calldidconnect(options?: any): Promise<void>;
-
-  calldiddisconnect(options?: any): Promise<void>;
-
-  
-
+    echo(options: {
+        value: string;
+    }): Promise<{
+        value: string;
+    }>;
 }
