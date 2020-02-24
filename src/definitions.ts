c37470dd1a61ee0d2723f1028d927b7a11ebfc40
mkw <michaelkweiner@gmail.com>
2020-02-24 10:49:36 -0800

    definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,5 +5,48 @@ declare module "@capacitor/core" {
 }
 
 export interface CapacitorTwilioVoiceSDKPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
+  echo(options: {
+    value: string;
+}): Promise<{
+    value: string;
+}>;
+
+
+call(options: {
+    token?: string;
+    params?: any;
+  }): Promise<{
+    value: string;
+  }>;
+
+  init(options?: any): Promise<{value: string}>;
+
+  sendDigits(digits?: string): Promise<{value: string}>;
+
+  disconnect(options?: any): Promise<void>;
+
+  rejectCallInvite(options?: any): Promise<void>;
+
+  acceptCallInvite(options?: any): Promise<void>;
+
+  setSpeaker(options?: any): Promise<void>;
+
+  muteCall(options?: any): Promise<void>;
+
+  unmuteCall(options?: any): Promise<void>;
+
+  isCallMuted(options?: any): Promise<void>;
+
+  error(options?: any): Promise<void>;
+
+  clientinitialized(options?: any): Promise<void>;
+
+  callinvitereceived(options?: any): Promise<void>;
+
+  callinvitecanceled(options?: any): Promise<void>;
+
+  calldidconnect(options?: any): Promise<void>;
+
+  calldiddisconnect(options?: any): Promise<void>;
+
 }
